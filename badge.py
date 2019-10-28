1edd40bd12ddda5b90190f64f9b4e15a7b8886f2
ChristfriedBalizou <christfriedbalizou@gmail.com>
2019-10-28 11:24:36 +0100

    change: remove image border radius

--- badge.py
+++ badge.py
@@ -16,13 +16,11 @@ BADGE_IMAGE_HEIGHT = 25
 set to 25px
 """
 
-RADIUS_WIDTH = 2
-
 WIDTH_PADDING = 5
 """Use to pad on right and left text
 """
 
-TEXT_PADDING_RATIO = 7
+TEXT_PADDING_RATIO = 10
 """Let's pretend that padding a single
 char require 5px to be correctly fit
 """
@@ -71,76 +69,18 @@ class ConcatStrategy(enum.Enum):
     VERTICAL = 2
 
 
-def radius_image(image, radius, *args):
-    """This function add a radius border to an
-    image
-
-    Inspired by @fraxel
-    https://stackoverflow.com/a/11291419
-
-    Args:
-        image (Image): a pillow image
-        radius (int): how much radius to curve
-        args (List[Border]): which side of the border to radius
-
-    Return: an image radius shaped
-    """
-
-    circle = Image.new("L", (radius * 2, radius * 2), 0)
-
-    draw = ImageDraw.Draw(circle)
-    draw.ellipse((0, 0, radius * 2, radius * 2), fill=255)
-
-    alpha = Image.new("L", image.size, 255)
-
-    width, height = image.size
-
-    for side in args:
-
-        if side == Border.LEFT_TOP:
-            alpha.paste(circle.crop((0, 0, radius, radius)), (0, 0))
-
-        if side == Border.LEFT_BOTTOM:
-            alpha.paste(
-                circle.crop((0, radius, radius, radius * 2)),
-                (0, height - radius),
-            )
-
-        if side == Border.RIGHT_TOP:
-            alpha.paste(
-                circle.crop((radius, 0, radius * 2, radius)),
-                (width - radius, 0),
-            )
-
-        if side == Border.RIGHT_BOTTOM:
-            alpha.paste(
-                circle.crop((radius, radius, radius * 2, radius * 2)),
-                (width - radius, height - radius),
-            )
-
-    image.putalpha(alpha)
-    return image
-
-
-def build_image(size, color=None, radius=None):
+def build_image(size, color=None):
     """This function create an image using the size
     and the color
 
     Args:
         size (Tuple[str, str]): width and height of the image
         color (Tuple[str, str, str]): RGB color code
-        radius (Tuple[int, List[str]]): radius information
 
     Return: an PIL.Image
     """
 
-    image = Image.new("RGB", size, color=color)
-
-    if radius:
-        radius_size, sides = radius
-        image = radius_image(image, radius_size, *sides)
-
-    return image
+    return Image.new("RGB", size, color=color)
 
 
 def set_text(image, label):
@@ -180,7 +120,6 @@ def badge_label(label, color=None):
     image = build_image(
         (width + WIDTH_PADDING, BADGE_IMAGE_HEIGHT),
         color=color,
-        radius=None
     )
 
     return set_text(image, label)
@@ -232,19 +171,11 @@ def badge(label, message, status):
 
     Return: PIL.Image
     """
-    image = concat_images(
+
+    return concat_images(
         ConcatStrategy.HORIZONTAL,
         badge_label(label, color=LABEL_BACKGROUND_COLOR),
-        badge_label(message, color=COLOR_PALLETTE[StatusColor(status)]),
-    )
-
-    return radius_image(
-        image,
-        RADIUS_WIDTH,
-        Border.LEFT_TOP,
-        Border.LEFT_BOTTOM,
-        Border.RIGHT_TOP,
-        Border.RIGHT_BOTTOM,
+        badge_label(message, color=COLOR_PALLETTE[StatusColor(status)])
     )
 
 
